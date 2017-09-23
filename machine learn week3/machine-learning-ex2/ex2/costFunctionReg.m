4f8cda141e28e6bfb86bc9d1a68118439b277e34
lonki <lonkiroom@gmail.com>
2017-09-24 00:35:36 +0800

    complete ex2

--- machine learn week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ machine learn week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -1,4 +1,5 @@
 function [J, grad] = costFunctionReg(theta, X, y, lambda)
+
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
@@ -18,10 +19,13 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+predictions = sigmoid(X * theta);
+J = -1 / m * (y' * log(predictions) + (1 - y') * log(1 - predictions)) + lambda /(2 * m) * (theta(2:end)' * theta(2:end));
 
 
+grad(1) = (1 / m) * (X(:,1)' * (predictions - y));
 
-
+grad(2:end) = ((1 / m) * (X(:,2:end)' * (predictions - y))) + lambda / m * theta(2:end);
 % =============================================================
 
 end
