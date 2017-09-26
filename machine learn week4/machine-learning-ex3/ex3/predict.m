ee845cd3c1298d754ac4b5a8770dec226477a471
zhujie <zhujie8721*>
2017-09-26 17:07:55 +0800

    complete ex3

--- machine learn week4/machine-learning-ex3/ex3/predict.m
+++ machine learn week4/machine-learning-ex3/ex3/predict.m
@@ -21,12 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m,1) X];
 
+z2 = X * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m,1) a2];
+z3 = a2 * Theta2';
+predictions = sigmoid(z3);
 
-
-
-
-
+[max_values indices] = max(predictions,[],2);
+p = indices;
 
 
 % =========================================================================
